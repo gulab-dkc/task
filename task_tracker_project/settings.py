f7aed781a38685a574364b53f9395266b73a6e0f
Gulabchandyadav800 <gulab.a1999@example.com>
2025-04-16 15:29:32 +0530

    update task details page

--- task_tracker_project/settings.py
+++ task_tracker_project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",  # Add this line
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "simple_history.middleware.HistoryRequestMiddleware",
